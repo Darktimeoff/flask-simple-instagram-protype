f367eaad0ce5ab77cf9aad1377348c6b8f617dc5
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-11-27 16:30:59 +0200

    create base server configuration

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+from dotenv import load_dotenv
+from logging import basicConfig
+from os import environ
+
+load_dotenv(override=True)
+basicConfig(filename='basic.log')
+
+app = Flask(__name__)
+
+if environ.get('APP_CONFIG', 'local') == 'local':
+    app.config.from_pyfile('config/development.py')
+else:
+    app.config.from_pyfile('config/production.py')
+
+app.run(debug=app.config.get('DEBUG', False))
